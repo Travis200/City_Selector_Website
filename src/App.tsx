a281cbe1c9292e2ba8ad3bf84f99c596c5a49643
Travis_Stuart_Higgins <t.higgins@spglobal.com>
2023-10-18 14:56:59 +0100

    Added alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
-const items = ["London", "Manchester", "Newcastle", "Edinburgh", "York"];
+import Alert from "./components/Alert";
 
-import ListGroup from "./components/ListGroup";
-
-function App() {
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+const App = () => {
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>Hello World</Alert>
     </div>
   );
-}
+};
 
 export default App;
+
+// const items = ["London", "Manchester", "Newcastle", "Edinburgh", "York"];
+
+// import ListGroup from "./components/ListGroup";
+
+// function App() {
+//   const handleSelectItem = (item: string) => {
+//     console.log(item);
+//   };
+//   return (
+//     <div>
+//       <ListGroup
+//         items={items}
+//         heading="Cities"
+//         onSelectItem={handleSelectItem}
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
